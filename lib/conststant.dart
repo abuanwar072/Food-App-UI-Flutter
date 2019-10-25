7933dcdf6be3b745f665dc2a22b6df3bc0e03cdc
Anwar Abir <abuanwar072@gmail.com>
2019-10-25 10:21:57 +0600

    Home Page Done

---
+++ lib/conststant.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+const Color kMainColor = Color(0xFF21BFBD);
+const Color kSecondaryColor = Color(0xFF7fa1fa);
+
+const TextStyle kHeading = TextStyle(
+  fontFamily: 'Montserrat',
+  color: Colors.white,
+  fontSize: 35,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle kHeadingLight = TextStyle(
+  fontFamily: 'Montserrat',
+  fontSize: 35,
+  color: Colors.white,
+  fontWeight: FontWeight.w300,
+);
+
+const TextStyle kHeadingSmall = TextStyle(
+  fontFamily: 'Montserrat',
+  color: Colors.black,
+  fontWeight: FontWeight.w700,
+  fontSize: 18,
+);
+
+const TextStyle kPriceText = TextStyle(
+  fontFamily: "Montserrat",
+  fontWeight: FontWeight.w700,
+  fontSize: 14,
+  color: Colors.grey,
+);
